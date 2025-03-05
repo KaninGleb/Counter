2a5063707e53c51f2f0da8718d46f3f387662571
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-05 23:35:09 +0300

    Counter - Old error handling removed

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -18,27 +18,7 @@ export const Counter = ( {startValue, maxValue, isEditing, error}: CounterType )
     const incrementHandler = () => setNum(prev => prev + 1);
     const resetHandler = () => setNum(startValue);
 
-    // const getErrorHandler = () => {
-    //     if (startValue >= maxValue) {
-    //         return 'Start value must be less than the maximum value';
-    //     } else if (startValue < 0 || maxValue < 0) {
-    //         return 'Values must be non-negative';
-    //     }
-    //     return false;
-    // }
-    //
-    // const errorMessage = getErrorHandler();
-
     const isDisabled = error !== null || isEditing || num >= maxValue;
-
-    // const isError = startValue >= maxValue || startValue < 0 || maxValue < 0;
-    // const errorMessage = isError
-    //     ? startValue < 0 || maxValue < 0
-    //         ? 'Values must be non-negative'
-    //         : 'Start value must be less than the maximum value'
-    //     : '';
-    //
-    // const isDisabled = isError || isEditing || num >= maxValue;
 
     return (
         <div className={'counter'}>
