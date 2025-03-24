a94f4d720a2b998b44bcb3b178fa94438ec1476c
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-24 22:13:41 +0300

    App - Logic of version switching changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import {useState} from 'react';
-import {Counter} from './components/Counter.tsx';
-import {CounterSettings} from './components/CounterSettings.tsx';
+import {AppWithState} from './AppWithState.tsx';
+import {AppWithReducer} from './AppWithReducer.tsx';
 import {VersionSwitcher} from './components/VersionSwitcher.tsx';
 
 
 function App() {
-    const initialValues = {
-        startValue: 0,
-        maxValue: 3,
-    }
-
-    const localStorageValues = () => {
-        const savedValues = localStorage.getItem('counterSettingsValues');
-        return savedValues ? JSON.parse(savedValues) : initialValues;
-    }
-
     const localStorageVersion = () => {
         const savedValues = localStorage.getItem('counterVersion');
         return savedValues ? savedValues : 'v1';
     }
 
-    const [values, setValues] = useState(localStorageValues);
-    const [isEditing, setIsEditing] = useState(false);
-    const [error, setError] = useState<string>('');
     const [version, setVersion] = useState(localStorageVersion);
-
-    const setValuesHandler = (newStartValue: number, newMaxValue: number) => {
-        const newValues = {startValue: newStartValue, maxValue: newMaxValue }
-        setValues(newValues);
-        localStorage.setItem('counterSettingsValues', JSON.stringify(newValues));
-    }
 
     const handleVersionChange = (newVersion: string) => {
         setVersion(newVersion);
@@ -39,31 +20,10 @@ function App() {
     return (
         <div className="App">
             {version === 'v1' ? (
-                <div className={"container"}>
-                    <CounterSettings
-                        startValue={values.startValue}
-                        maxValue={values.maxValue}
-                        onSetValues={setValuesHandler}
-                        onEdit={setIsEditing}
-                        onError={setError}
-                        error={error}
-                    />
-                    <Counter
-                        startValue={values.startValue}
-                        maxValue={values.maxValue}
-                        isEditing={isEditing}
-                        error={error}
-                    />
-                </div>
+                <AppWithState/>
             ) : (
-                <Counter
-                    startValue={values.startValue}
-                    maxValue={values.maxValue}
-                    isEditing={isEditing}
-                    error={error}
-                />
+                <AppWithReducer/>
             )}
-
             <VersionSwitcher currentVersion={version} onVersionChange={handleVersionChange}/>
         </div>
     )
