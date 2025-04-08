33c2a3f702635f219fca0b7f8d3bdcc75b14ab70
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-08 23:30:37 +0300

    localStorage.ts disabled

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -1,20 +1,20 @@
-export const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('currentValue');
-    if (serializedState === null) {
-      return undefined;
-    }
-    return JSON.parse(serializedState);
-  } catch {
-    return undefined;
-  }
-}
-
-export const saveState = (state) => {
-  try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('currentValue', serializedState);
-  } catch {
-    // Ignore
-  }
-}
+// export const loadState = () => {
+//   try {
+//     const serializedState = localStorage.getItem('currentValue');
+//     if (serializedState === null) {
+//       return undefined;
+//     }
+//     return JSON.parse(serializedState);
+//   } catch {
+//     return undefined;
+//   }
+// }
+//
+// export const saveState = (state) => {
+//   try {
+//     const serializedState = JSON.stringify(state);
+//     localStorage.setItem('currentValue', serializedState);
+//   } catch {
+//     // Ignore
+//   }
+// }
