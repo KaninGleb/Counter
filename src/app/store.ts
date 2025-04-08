1cf13d520a206ee040ec044e588c9703d54a4c20
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-08 23:14:49 +0300

    Store - future update added

--- src/app/store.ts
+++ src/app/store.ts
@@ -13,3 +13,29 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch;
+
+
+
+
+
+// import {throttle} from 'lodash';
+//
+// const rootReducer = combineReducers({
+//   counterV3: counterV3Reducer,
+// });
+//
+// const persistedState = loadState();
+//
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   preloadedState: persistedState,
+// });
+//
+// store.subscribe(throttle(() => {
+//   saveState({
+//     counterV3: store.getState().counterV3,
+//   });
+// }, 1000));
+//
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
