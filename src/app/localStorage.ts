822acab4576f16494aa115d66a27a28c20c9acf9
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-12 16:18:27 +0300

    Redux localStorage loadState and saveState functions added

--- src/app/localStorage.ts
+++ src/app/localStorage.ts
@@ -1,20 +1,27 @@
-// export const loadState = () => {
-//   try {
-//     const serializedState = localStorage.getItem('currentValue');
-//     if (serializedState === null) {
-//       return undefined;
-//     }
-//     return JSON.parse(serializedState);
-//   } catch {
-//     return undefined;
-//   }
-// }
-//
-// export const saveState = (state) => {
-//   try {
-//     const serializedState = JSON.stringify(state);
-//     localStorage.setItem('currentValue', serializedState);
-//   } catch {
-//     // Ignore
-//   }
-// }
+
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.log('Could not load state:', err);
+    return undefined;
+  }
+}
+
+export const saveState = (state: saveStateType) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    console.log('Could not save state:', err);
+  }
+}
+
+type saveStateType = {
+  counterV3: number
+}
