e5ebfdb5287edb071aa29915085fab50b7d0495c
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-12 16:18:44 +0300

    Redux store implemented with localStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,41 +1,23 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {counterV3Reducer} from '@/features/model/counterV3Reducer.ts';
+import {loadState, saveState} from '@/app/localStorage.ts';
 
 
 const rootReducer = combineReducers({
   counterV3: counterV3Reducer
 })
 
+const persistedState = loadState();
+
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  preloadedState: persistedState,
+})
+
+store.subscribe(() => {
+  saveState(store.getState());
 })
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch;
-
-
-
-
-
-// import {throttle} from 'lodash';
-//
-// const rootReducer = combineReducers({
-//   counterV3: counterV3Reducer,
-// });
-//
-// const persistedState = loadState();
-//
-// export const store = configureStore({
-//   reducer: rootReducer,
-//   preloadedState: persistedState,
-// });
-//
-// store.subscribe(throttle(() => {
-//   saveState({
-//     counterV3: store.getState().counterV3,
-//   });
-// }, 1000));
-//
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
